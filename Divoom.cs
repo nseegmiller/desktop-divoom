07ce299c2dd46b5a5c2abc887a32ec338b77a0a2
Nick <nick@seegy.net>
2023-07-16 17:59:45 -0700

    Project setup with initial classes

---
+++ Divoom.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace DesktopDivoom
+{
+    public class Divoom
+    {
+        static HttpClient client = new();
+        const string HTTP = "http://";
+        const string POST = "/post";
+        const string TEST_PAYLOAD = "Device/GetWeatherInfo";
+        const string PLAY_GIF_PAYLOAD = "Device/PlayTFGIF";
+
+        public static void Initialize()
+        {
+            //client.BaseAddress = new Uri("http://" + ipAddress + ":80");
+            if (client == null)
+            {
+                client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+        }
+
+        private static async Task SendPayload(string ipAddress, Payload payload, Action<HttpResponseMessage> callback)
+        {
+            Initialize();
+            string path = HTTP + ipAddress + POST;
+            try
+            {
+                HttpResponseMessage response = await client.PostAsJsonAsync(path, payload);
+                callback(response);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                callback(new HttpResponseMessage { StatusCode = HttpStatusCode.InternalServerError });
+            }
+        }
+
+        public static Task TestConnection(string ipAddress, Action<HttpResponseMessage> callback)
+        {
+            Payload testPayload = new()
+            {
+                Command = TEST_PAYLOAD
+            };
+            _ = SendPayload(ipAddress, testPayload, callback);
+            return Task.CompletedTask;
+        }
+
+        public static Task SendGif(string ipAddress, string fileName, Action<HttpResponseMessage> callback)
+        {
+            Payload testPayload = new()
+            {
+                Command = PLAY_GIF_PAYLOAD,
+                FileType = 2,
+                FileName = fileName,
+            };
+            _ = SendPayload(ipAddress, testPayload, callback);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopDivoom
+{
+
+
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Divoom divoom = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async Task TestConnection()
+        {
+            testResponse.Foreground = Brushes.Black;
+            testResponse.Text = "Searching...";
+            testConnectionButton.IsEnabled = false;
+            await Divoom.TestConnection(ipAddress.Text, HandleTestResponse);
+            testConnectionButton.IsEnabled = true;
+        }
+
+        private void Test_Connection_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("IP Address: " + ipAddress.Text);
+            _ = TestConnection();
+        }
+
+        private void sendGifButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sendGifURI.Text.Length > 0)
+            {
+                Debug.WriteLine("Sending URL at: " + sendGifURI.Text);
+                Divoom.SendGif(ipAddress.Text, sendGifURI.Text, HandleGifSendResponse);
+            }
+            else
+            {
+                sendGifResponse.Foreground = Brushes.Red;
+                sendGifResponse.Text = "Invalid URL";
+            }
+        }
+
+        private void HandleGifSendResponse(HttpResponseMessage response)
+        {
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                sendGifResponse.Foreground = Brushes.Green;
+                sendGifResponse.Text = "GIF sent to device.";
+            }
+            else if (response.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                sendGifResponse.Foreground = Brushes.Red;
+                sendGifResponse.Text = "Error sending GIF.";
+            }
+        }
+
+        private void HandleTestResponse(HttpResponseMessage response)
+        {
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Debug.WriteLine("Device Found!");
+                testResponse.Foreground = Brushes.Green;
+                testResponse.Text = "Device Found!";
+            }
+            else if (response.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                testResponse.Foreground = Brushes.Red;
+                testResponse.Text = "Invalid IP Address.";
+            }
+            else
+            {
+                Debug.WriteLine("Device Not Found.");
+                testResponse.Foreground = Brushes.Red;
+                testResponse.Text = "Device Not Found.";                
+            }       
+        }
+
+        private void IP_Address_Changed(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
