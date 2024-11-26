9507943b1fac51c145f69d62cf7a730d9f50d269
Nick Seegmiller <nick.seegmiller@gmail.com>
2024-11-26 09:33:22 -0800

    Removed usage of PostAsJsonAsync
    It actually works now

--- Divoom.cs
+++ Divoom.cs
@@ -4,6 +4,8 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DesktopDivoom
@@ -13,28 +15,26 @@ namespace DesktopDivoom
         static HttpClient client = new();
         const string HTTP = "http://";
         const string POST = "/post";
-        const string TEST_PAYLOAD = "Device/GetWeatherInfo";
-        const string PLAY_GIF_PAYLOAD = "Device/PlayTFGIF";
+        const string TEST_COMMAND = "Device/GetWeatherInfo";
+        const string PLAY_GIF_COMMAND = "Device/PlayTFGif";
 
-        public static void Initialize()
+        private static StringContent MakePayload(Object content)
         {
-            //client.BaseAddress = new Uri("http://" + ipAddress + ":80");
-            if (client == null)
-            {
-                client = new HttpClient();
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-            }
+            StringContent payload = new(
+                JsonSerializer.Serialize(content),
+                Encoding.UTF8,
+                "application/json");
+            return payload;
         }
 
-        private static async Task SendPayload(string ipAddress, Payload payload, Action<HttpResponseMessage> callback)
+        private static async Task SendPayload(string ipAddress, StringContent payload, Action<HttpResponseMessage> callback)
         {
-            Initialize();
             string path = HTTP + ipAddress + POST;
             try
             {
-                HttpResponseMessage response = await client.PostAsJsonAsync(path, payload);
+                HttpResponseMessage response = await client.PostAsync(path, payload);
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
                 callback(response);
             }
             catch (Exception ex)
@@ -46,23 +46,23 @@ namespace DesktopDivoom
 
         public static Task TestConnection(string ipAddress, Action<HttpResponseMessage> callback)
         {
-            Payload testPayload = new()
+            StringContent payload = MakePayload(new
             {
-                Command = TEST_PAYLOAD
-            };
-            _ = SendPayload(ipAddress, testPayload, callback);
+                Command = TEST_COMMAND
+            });
+            _ = SendPayload(ipAddress, payload, callback);
             return Task.CompletedTask;
         }
 
         public static Task SendGif(string ipAddress, string fileName, Action<HttpResponseMessage> callback)
         {
-            Payload testPayload = new()
+            StringContent payload = MakePayload(new
             {
-                Command = PLAY_GIF_PAYLOAD,
+                Command = PLAY_GIF_COMMAND,
                 FileType = 2,
-                FileName = fileName,
-            };
-            _ = SendPayload(ipAddress, testPayload, callback);
+                FileName = fileName
+            });
+            _ = SendPayload(ipAddress, payload, callback);
             return Task.CompletedTask;
         }
     }
